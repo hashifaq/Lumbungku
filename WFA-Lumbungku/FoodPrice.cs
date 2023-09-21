1e0c15aea154c265b2f20e5c62425b723b3f89d5
Maulana-anjari <maulana17anjari@gmail.com>
2023-09-21 15:38:28 +0700

    Menambahkan class FoodPrice dan class Weather

---
+++ WFA-Lumbungku/FoodPrice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFA_Lumbungku
+{
+    internal class FoodPrice
+    {
+        static string DataSetUrl = "https://sementarabelumtahu";
+        internal struct DataFood
+        {
+            int item_id;
+            string name;
+            int price;
+            string unit;
+            DateTime date;
+        }
+        public FoodPrice()
+        {
+            getFoodPrice();
+        }
+        public void getFoodPrice()
+        {
+            
+        }
+    }
+}
+
